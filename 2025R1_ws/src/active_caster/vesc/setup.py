7c8beefb8834f681e7a1db30979e20f50c0234fe
ZHANG YANCHENG <yanczhang8@gmail.com>
2025-05-16 01:57:07 +0800

    update vesc/setup reqirement declare

--- 2025R1_ws/src/active_caster/vesc/setup.py
+++ 2025R1_ws/src/active_caster/vesc/setup.py
@@ -6,12 +6,16 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    install_requires=[
+        'setuptools',
+        'pyserial>=3.0,<4.0',
+        'pyvesc>=1.0.5',
+    ],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='steven',
     maintainer_email='yanczhang8@gmail.com',
