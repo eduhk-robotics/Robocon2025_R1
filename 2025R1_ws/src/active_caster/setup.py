24f54073328fd3e3b42068d6c61a2a093fd91c1f
ZHANG YANCHENG <yanczhang8@gmail.com>
2025-05-20 04:49:50 +0800

    Modify setup of active_caster

--- 2025R1_ws/src/active_caster/setup.py
+++ 2025R1_ws/src/active_caster/setup.py
@@ -6,6 +6,11 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    install_requires=[
+        'setuptools',
+        'pyserial>=3.0,<4.0',
+        'pyvesc>=1.0.5',
+    ],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
